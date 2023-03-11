8c6d1ab9adbe91a4aae1e65e3e399088a272e5c5
hjk200 <84971184+hjk200@users.noreply.github.com>
2023-03-11 14:16:27 +0100

    premier commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 window.onload = () => {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  const img = new Image();
+
+  img.src = '/Users/henri/Desktop/lab-canvas-race-car/images/road.png';
+
+  img.onload = () => {
+    ctx.drawImage(img, 0, 0);
+  };
+
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-
-  function startGame() {}
+  function startGame() {
+    console.log('le jeu a commence !');
+  }
 };
